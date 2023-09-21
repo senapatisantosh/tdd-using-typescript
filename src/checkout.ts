ff503dcb0c2569bfd73fdb71323bbe2f30a99b10
Santosh Kumar Senapati <Santosh.Senapati@euromonitor.com>
2023-09-21 13:05:44 +0530

    iteration 2

--- src/checkout.ts
+++ src/checkout.ts
@@ -1,3 +1,11 @@
 export function checkout(itemList: string): number {
-    return 0;
+    let total = 0;
+    for (const item of itemList.split('')) {
+        if (item === 'A') total += 50;
+        else if (item === 'B') total += 30;
+        else if (item === 'C') total += 20;
+        else if (item === 'D') total += 15;
+        else throw new Error("Unknown Item");
+    }
+    return total;
 }
--- test/checkout.test.ts
+++ test/checkout.test.ts
@@ -1,7 +1,28 @@
 import { checkout } from "../src/checkout";
 
-describe('checkout', () => {
-    it('cbd', () => {
+describe('checkout test', () => {
+    it('Should return 0 when no items are sent', () => {
         expect(checkout('')).toBe(0);
+    });
+    it('Should return 50 when sending an A', () => {
+        expect(checkout('A')).toBe(50);
+    });
+    it('Should return 30 when sending a B', () => {
+        expect(checkout('B')).toBe(30);
+    });
+    it('Should return 20 when sending a C', () => {
+        expect(checkout('C')).toBe(20);
+    });
+    it('Should return 15 when sending a D', () => {
+        expect(checkout('D')).toBe(15);
+    });
+    it('Should return 100 when sending AA', () => {
+        expect(checkout('AA')).toBe(100);
+    });
+    it('Should return 115 when sending ABCD', () => {
+        expect(checkout('ABCD')).toBe(115);
+    });
+    it('Should throw an error when sending an Unknown item', () => {
+        expect(() => checkout('Z')).toThrow('Unknown Item');
     });
 })
